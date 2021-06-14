c0ac1874daac66d2c1d6e783bab84eff001e7a99
31AOmkarA <58028247+Omkar-Chaskar@users.noreply.github.com>
2021-06-14 09:51:34 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+
+var readlineSync = require("readline-sync");
+
+let i,j,k;
+
+// Star Pattern
+/*
+let star = "";
+
+let n = readlineSync.question("Enter no. to draw upward star pattern = ")
+for(i=0;i<n;i++){
+console.log(star += "*");
+}
+
+let m = readlineSync.question("Enter no. to draw downward star pattern = ")
+
+for(j=0;j<m;j++){
+  for(k=0;k<m-j;k++){
+  star += "*";
+  }
+  star += "\n";
+  }
+  console.log(star)
+  */
+
+  //  list grocery items to buy
+  /*
+   var groceryList = ["Bhendi","Gajar","Batata","Wangi","Karli","Bhopala","Shepu"];
+   console.log(groceryList);
+   console.log(groceryList[1]);
+   console.log(groceryList.length)
+
+   for(i=0;i<groceryList.length;i++)
+   console.log(groceryList[i])
+*/
+
+
+  
